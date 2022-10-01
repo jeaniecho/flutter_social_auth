f74f7df68b4646c461c47c6d85ad326f61e14f4e
Jeanie <whdpwls63@gmail.com>
2022-10-01 19:27:06 +0900

    added apple login

--- lib/auth/apple_auth.dart
+++ lib/auth/apple_auth.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_social_auth/common/size.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
-class AppleAuth extends StatelessWidget {
+class AppleAuth extends StatefulWidget {
   const AppleAuth({Key? key}) : super(key: key);
+
+  @override
+  State<AppleAuth> createState() => _AppleAuthState();
+}
+
+class _AppleAuthState extends State<AppleAuth> {
+  Future<bool> signIn() async {
+    try {
+      if (await SignInWithApple.isAvailable()) {
+        final AuthorizationCredentialAppleID credential = await SignInWithApple.getAppleIDCredential(
+            scopes: [
+              AppleIDAuthorizationScopes.fullName,
+              AppleIDAuthorizationScopes.email,
+            ],
+        );
+
+        debugPrint('credential.state = $credential');
+        debugPrint('credential.state = ${credential.email}');
+        debugPrint('credential.state = ${credential.userIdentifier}');
+
+        return true;
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () {print("apple");},
+      onPressed: () async {
+        String title = "Apple Sign In";
+        String content = await signIn() ? "Successfully signed in!" : "Sign in failed :(";
+
+        showDialog(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: Text(title),
+            content: Text(content),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'OK'),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
+      },
       icon: Image.asset(
         'assets/apple_icon.png',
         fit: BoxFit.cover,
@@ -16,4 +61,5 @@ class AppleAuth extends StatelessWidget {
     );
   }
 }
+
 
